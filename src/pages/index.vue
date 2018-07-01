52ed370d98135892c22ac2bfd9335c1b0f7a98ff
F9Uf <atoseka@gmail.com>
2018-07-01 15:56:28 +0700

    add bootstrap

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,7 @@
       <nuxt-link to="/login">LOGIN</nuxt-link> | 
       <nuxt-link to="/admin">ADMIN</nuxt-link>
     </div>
+    <button class="btn btn-success">test</button>
   </section>
 </template>
 
